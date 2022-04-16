1d9f92a5ba8fa4c730a080fa0311db91c9dece5d
davide1217 <davidepizzola17@gmail.com>
2022-04-16 11:38:09 +0200

    jsnack1-2-3(incluso-while)

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,18 +1,46 @@
 const array = [];
-let sum = 0
-for(i = 0; i < 5; i++) {
+let control;
+let sum = 0;
+const ripetizioni = 5;
+
+for(i = 0; i < ripetizioni; i++) {
   array.push(prompt('inserisci un numero'))
-  
+
   if(array[i].includes(',')){
-    alert('use \'.\' instead of \',\'')
-    array.pop()
-    array.push(prompt('inserisci un numero'))
+    for(array[i]; array[i].includes(','); array.push(prompt('inserisci un numero'))){
+      alert('use \'.\' instead of \',\'')
+      array.pop()
+    }
   }
+
+  sum += parseFloat(array[i])
 };
 
-for(i = 0; i < array.length; i++) {
+document.querySelector('div').innerHTML = sum;
+
+
+/*let i = 0;
+
+while(i < ripetizioni){
+
+  array.push(prompt('inserisci un numero'))
+  control += parseFloat(array[i])
+
+  if(array[i].includes(',')) {
+    while(array[i].includes(',')){
+      alert('use \'.\' instead of \',\'')
+      array.pop()
+      array.push(prompt('inserisci un numero'))
+    } 
+  }
+  
+
   sum += parseFloat(array[i])
+
+  i++
 }
 
-document.querySelector('div').innerHTML = sum;
-console.log();
+document.querySelector('div').innerHTML = sum;*/
+
+
+
