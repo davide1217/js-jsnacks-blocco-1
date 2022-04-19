fa40b1928f812d5768e5751bcd1cda894ccf57ae
davide1217 <davidepizzola17@gmail.com>
2022-04-19 18:26:19 +0200

    jsnack1-2-3-4(no-loop)

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -2,15 +2,16 @@ const array = ['giacomino','ugo','lello'];
 const nome = prompt('nome');
 const nomeCorretto = nome.toLowerCase();
 
-let i = 0;
+let presente = false;
+let i = -1;
 do {
+
+   i++
    if(nomeCorretto === array[i]){
    alert(`Mr ${nome}, you are welcome.`);
- } else if(i === array.length - 1 && nomeCorretto !== array[i]){
+   presente = true;
+ } else if(i === array.length - 1 && nomeCorretto != array[i]){
    alert(`Mr ${nome}, you are not invited.`)
- } else {
-   i++
-  }
- 
-} while(i < array.length - 1 || nomeCorretto !== array[i])
+ }
 
+} while(i < array.length && !presente)
